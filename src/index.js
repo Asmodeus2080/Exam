6c444c8158e18a93eb1d39d7f8eb002043450bed
Shivam <shivamnegi478@gmail.com>
2024-02-25 03:19:05 +0530

    cors done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = require('./routes');
 const { serverConfig, database } = require('./config');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors());
 
 app.use('/api', router);
 
