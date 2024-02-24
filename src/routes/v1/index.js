eda13c4323c6ce2df49fe1d0f36fa547172d9de1
Shivam <shivamnegi478@gmail.com>
2024-02-25 04:18:21 +0530

    changed index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,10 +5,12 @@ const SubtopicRoutes = require('./subtopic-routes');
 const StudyMaterialRoutes = require('./studymaterial-routes');
 const UserRoutes = require('./user-routes');
 const QuestionBankRoutes  = require('./questionbank-routes');
+const RoadmapRoutes = require('./roadmap-routes');
 
 router.use('/subtopic', SubtopicRoutes);
 router.use('/studyMaterial', StudyMaterialRoutes);
 router.use('/user', UserRoutes);
 router.use('/questionBank', QuestionBankRoutes);
+router.use('/roadmap', RoadmapRoutes);
 
 module.exports = router;
