ca13056d91ee806ff9782e04e702a4147398ebcf
Shivam <shivamnegi478@gmail.com>
2024-02-25 00:21:20 +0530

    removed duplicates

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 const SubtopicRoutes = require('./subtopic-routes');
 const StudyMaterialRoutes = require('./studymaterial-routes');
-
-router.use('/studyMaterial', StudyMaterialRoutes);
-router.use('/subtopic', SubtopicRoutes);
 const UserRoutes = require('./user-routes');
-const StudyMaterialRoutes = require('./studymaterial-routes');
 
-router.use('/studyMaterial', StudyMaterialRoutes);
 router.use('/subtopic', SubtopicRoutes);
+router.use('/studyMaterial', StudyMaterialRoutes);
 router.use('/user', UserRoutes);
 
 module.exports = router;
